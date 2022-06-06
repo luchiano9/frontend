7d93fd15a64fae2428c09d6455fc9aaf4becaef8
Luciano Vannelli <lucianovannelli@gmail.com>
2022-06-06 12:00:25 -0300

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+
+import memories from "./images/memories.png"
 
 const App = () => {
-    return (<div>
-        <h1>App</h1>
-    </div>)
-}
+  return (
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">Memories</Typography>
+          <img src={memories} alt="memories" height ="210"/>
+
+      </AppBar>
+      <Grow in >
+          <Container>
+              <Grid container justifyContent ="space-between" align-items="stretch" spacing={3}>
+
+                  <Grid item xs={12} sm={7}>
+                    <Posts/>
+                  </Grid>
+                  
+                  <Grid item xs={12} sm={4}>
+                      <Form/>
+                  </Grid>
+              </Grid>
+          </Container>
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
